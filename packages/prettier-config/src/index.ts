a464a2163df6ec15d7acfec1f295a33c5a7a3550
buames <716665+buames@users.noreply.github.com>
2021-06-03 08:33:23 -0700

    More prettier-config to ts

---
+++ packages/prettier-config/src/index.ts
@@ -0,0 +1,19 @@
+import type { Options } from 'prettier';
+
+const config: Options = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: 'always',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
+
+export default config;
